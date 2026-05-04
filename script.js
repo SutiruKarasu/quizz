351cf049e40179d2692daa2a9179f7f4781283de
SutiruKarasu <kolgeciedi5@gmail.com>
2026-05-04 17:03:27 +0200

    Add scoring functionality to answer function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let score = 0
+function answer(choice)  {
+  let result = document.getElementById("result");
 
+  if (choice === 1)  {
+    result.innertext = "correct";
+    score++;
+  } else {
+    result.innerText = "Wrong";
+
+  }
+
+  console.log("Score:", score);
+
+}
